48b04d44f1677b8d2892579063ead4632dce330a
SEM <SEM@DESKTOP-4GKM6HK>
2022-11-04 08:55:45 +0300

    Add project files.

---
+++ Scope3/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Basla();
+
+void Basla()
+{
+    double x = 100;
+    Console.WriteLine("x ilk değeri:" + x);
+    x = Kokal(x);
+    Console.WriteLine("x metot sonrası değeri:" + x);
+
+}
+
+double Kokal(double x)
+{
+    return Math.Sqrt(x);
+}
